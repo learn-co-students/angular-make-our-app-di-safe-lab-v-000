d35db1e7784313efb64a6cde873c9bc6e9550439
shinyrachel <rachel.kory@gmail.com>
2016-05-17 18:16:06 -0400

    complete

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,10 +1,12 @@
-function ContactController($scope, $timeout) {
-	$scope.name = 'Bill Gates';
+function ContactController(something, anything) {
+	something.name = 'Bill Gates';
 
-	$timeout(function () {
+	anything(function () {
 		$scope.name = 'Steve Jobs';
 	}, 5000);
 }
+
+ContactController.$inject = ['$scope', '$timeout'];
 
 angular
 	.module('app')
