82538be4c66541fed76889edc88ff841914ccba8
authorbeard <msnolan@gmail.com>
2016-05-19 08:57:16 -0400

    all specs passing

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,10 +1,12 @@
-function ContactController($scope, $timeout) {
-	$scope.name = 'Bill Gates';
+function ContactController(deez, nutz) {
+	var deez = $scope.name = 'Bill Gates';
 
-	$timeout(function () {
+	var nutz = $timeout(function () {
 		$scope.name = 'Steve Jobs';
 	}, 5000);
 }
+
+ContactController.$inject=['$scope', '$timeout']
 
 angular
 	.module('app')
