3e25330c4e2be96207ad67d008dee2b55a33e1c1
Saul <digitalh2o@gmail.com>
2016-10-04 21:52:59 -0500

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,10 +1,12 @@
-function ContactController($scope, $timeout) {
-	$scope.name = 'Bill Gates';
+function ContactController(derp, burp) {
+	derp.name = 'Bill Gates';
 
-	$timeout(function () {
-		$scope.name = 'Steve Jobs';
+	burp(function () {
+		derp.name = 'Steve Jobs';
 	}, 5000);
 }
+
+ContactController.$inject = ["$scope", "$timeout"];
 
 angular
 	.module('app')
